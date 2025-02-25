a2527c0922a29557c0fa214e575dc2c5fae9d337
lilianlacerda <lilianraiany@gmail.com>
2025-02-25 19:39:45 -0300

    Adição do código da aula 1

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello, World!");
+
+String firstName = "Lilian Raiany";
+String lastName = "Lacerda";
+String surname = "Lima";
+
+Console.WriteLine("Meu nome completo é " + firstName + surname + lastName);
+
+int idade = 26;
+
+//O cifrão faz com que a gente consiga escrever as variáveis dentro das ""
+Console.WriteLine($"Meu nome completo é {firstName} {surname} {lastName} e a minha idade é {idade}");
+
+Console.WriteLine("Escreva seu nome");
+
+//ReadLine é para receber resposta do usuário
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Olá {nome}, boa noite!!!");
+
+//Espera digitar alguma tecla para finalizar
+Console.ReadKey();
