194d97e36d86e3d6ba4090e07c0ad602dc652e9c
lilianlacerda <lilianraiany@gmail.com>
2025-03-07 21:14:28 -0300

    criação da classe ContaCorrente

---
+++ ContaCorrente/ContaCorrente/ContaCorrente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoLilian
+{
+    internal class ContaCorrente
+    {
+        public int Numero;
+        public string Titular;
+        public double Saldo;
+
+        public double Depositar(double ValorDeposito)
+        {
+            Saldo += ValorDeposito;
+            return Saldo;
+        }
+
+        public double Sacar(double ValorSaque)
+        {
+            Saldo -= ValorSaque;
+            return Saldo;
+        }
+
+        public string ExibirSaldo()
+        {
+            return $"Saldo Atual: {Saldo}";
+        }
+    }
+}
